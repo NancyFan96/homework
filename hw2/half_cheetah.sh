cbaab3505de6e2a7ee402a7979091c57b56d471d
Nancy Fan <hmfnj1996@gmail.com>
2018-09-19 07:55:04 -0700

    finish prob 8, edit comments

--- hw2/half_cheetah.sh
+++ hw2/half_cheetah.sh
@@ -6,18 +6,18 @@ source ../hw1/bin/activate
 # r: 0.005, 0.01, 0.02
 
 
-#for b in 10000 30000 50000
-#do
-#    for r in 0.005 0.01 0.02
-#    do
-#        tailtag="b$b""_r$r"
-#        echo "b = $b, r = $r, tail_tag = $tailtag"
-#        cmd="python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 \
-#            -s 32 -b $b -lr $r -rtg --nn_baseline --exp_name hc_$tailtag"
-#        echo $cmd
-#        eval $cmd
-#    done
-#done
+for b in 10000 30000 50000
+do
+    for r in 0.005 0.01 0.02
+    do
+        tailtag="b$b""_r$r"
+        echo "b = $b, r = $r, tail_tag = $tailtag"
+        cmd="python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 \
+            -s 32 -b $b -lr $r -rtg --nn_baseline --exp_name hc_$tailtag"
+        echo $cmd
+        eval $cmd
+    done
+done
 
 # PLOT & COMPARE
 #python plot.py data/hc_b10000_r0.005_HalfCheetah-v2_18-09-2018_19-43-39 \
@@ -41,22 +41,21 @@ source ../hw1/bin/activate
 #    data/hc_b50000_r0.02_HalfCheetah-v2_18-09-2018_22-39-10 --value MaxReturn
 
 
-
 bb=50000
 rr=0.02
-baserun="python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 \
-    -l 2 -s 32 -b $bb -lr $rr"
-tailtag="b$b""_r$r"
+baserun="python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b $bb -lr $rr"
+tailtag="b$bb""_r$rr"
 
 echo $baserun --exp_name hc_$tailtag
 echo $baserun -rtg --exp_name hc_rtg_$tailtag
 echo $baserun --nn_baseline --exp_name hc_nnb_$tailtag
 echo $baserun -rtg --nn_baseline --exp_name hc_rtg_nnb_$tailtag
 
-#eval $baserun --exp_name hc_$tailtag
-#eval $baserun -rtg --exp_name hc_rtg_$tailtag
-#eval $baserun --nn_baseline --exp_name hc_nnb_$tailtag
-#eval $baserun -rtg --nn_baseline --exp_name hc_rtg_nnb_$tailtag
+eval $baserun --exp_name hc_$tailtag
+eval $baserun -rtg --exp_name hc_rtg_$tailtag
+eval $baserun --nn_baseline --exp_name hc_nnb_$tailtag
+eval $baserun -rtg --nn_baseline --exp_name hc_rtg_nnb_$tailtag
+
 
 #python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 \
 #    -l 2 -s 32 -b <b*> -lr <r*> --exp_name hc_b<b*>_r<r*>
